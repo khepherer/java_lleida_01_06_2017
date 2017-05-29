c12c5961426d2b2639cf7a4590d09ccb8b103e9f
iconojdiazch <khepherer@gmail.com>
2017-05-29 13:44:34 +0200

    serviciosweb actualizado2

---
+++ serviciosweb/Cache/CacheWsMaven/src/main/java/ws/CacheWebService.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws;
+
+import cache.Cache;
+import cache.MemoriaCache;
+import java.util.Date;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author usuario
+ */
+@WebService(serviceName = "CacheWebService")
+public class CacheWebService {
+
+ private Cache<String, Date> cache = new MemoriaCache();
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "cache")
+    public String mensajeEnCache(@WebParam(name = "mensaje") String txt) {
+        Date valor = null;
+        if ((valor = cache.get(txt)) == null) {
+            valor = new Date();
+            cache.put(txt, valor);
+        }
+        return "Mensaje " + txt + " recibido a las " + valor;
+    }
+}
---
+++ serviciosweb/README.md
@@ -0,0 +1,3 @@
+# serviciosweb
+Ejemplos y materiales del curso de servicios Web
+Contiene zips (carpetas RestJEE y SoapAsync)
