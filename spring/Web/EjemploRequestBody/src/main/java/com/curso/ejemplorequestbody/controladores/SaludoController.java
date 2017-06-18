537d5b876d11317a84634b9173e44fdf8d191fa9
iconojdiazch <khepherer@gmail.com>
2017-06-18 13:53:55 +0200

    Ejemplos dao Hibernate actualizados2

--- spring/Web/EjemploRequestBody/src/main/java/com/curso/ejemplorequestbody/controladores/SaludoController.java
+++ spring/Web/EjemploRequestBody/src/main/java/com/curso/ejemplorequestbody/controladores/SaludoController.java
@@ -5,6 +5,9 @@
  */
 package com.curso.ejemplorequestbody.controladores;
 
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
