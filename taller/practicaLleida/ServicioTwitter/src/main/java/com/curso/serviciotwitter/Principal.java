d92d9bb758aefd6e1e8906777b60e44bc91cd000
iconojdiazch <khepherer@gmail.com>
2017-06-24 12:32:29 +0200

    Servicio Twitter actualizado con @SpringBootApplication

--- taller/practicaLleida/ServicioTwitter/src/main/java/com/curso/serviciotwitter/Principal.java
+++ taller/practicaLleida/ServicioTwitter/src/main/java/com/curso/serviciotwitter/Principal.java
@@ -6,18 +6,20 @@
 package com.curso.serviciotwitter;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  *
  * @author usuario
  */
+@SpringBootApplication
 public class Principal {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        SpringApplication.run(Config.class);
+        SpringApplication.run(Principal.class);
     }
 
 }
